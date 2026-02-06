85aa665b9e541c9c2be9a7c7b9609ea57c239321
Anirudh Sharma <therealanirudhsharma@gmail.com>
2026-02-06 23:52:19 +0530

    mvp completed

--- services/auth-service/init.sql
+++ services/auth-service/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
-  id UUID PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name VARCHAR(255) DEFAULT 'User',
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   role VARCHAR(50) DEFAULT 'customer',
